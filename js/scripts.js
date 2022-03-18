509673c58fc99b847c16f23b9e1abf1f54168f4a
Jonathan Cheng <jcheng461@gmail.com>
2022-03-18 21:32:26 +0900

    function now takes in a word as parameter. First character is removed. That same character is concatenated to the end of the word.

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,25 @@
 
 // Business logic
 function pigLatin(word) {
-  if (
-    word[0] === "a" ||
-    word[0] === "e" ||
-    word[0] === "i" ||
-    word[0] === "o" ||
-    word[0] === "u"
-  ) {
-    return true;
-  } else {
-    return false;
-  }
+  let firstCharRemoved = word;
+  let translatedWord;
+  // if (
+  //   word[0] === "a" ||
+  //   word[0] === "e" ||
+  //   word[0] === "i" ||
+  //   word[0] === "o" ||
+  //   word[0] === "u"
+  // ) {
+  // Remove character from the string.
+  firstCharRemoved = word.slice(1);
+  console.log("After removing the first character: " + firstCharRemoved);
+  // Concatenate this character to the end.
+  translatedWord = firstCharRemoved.concat(word[0]);
+  console.log("After attaching first char. to end: " + translatedWord);
+  return true;
+  // } else {
+  //   return false;
+  // }
 }
 
 // User Interface logic
