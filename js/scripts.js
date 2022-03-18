a7a57147461a658aab84915cc3df83e377e58b95
Jonathan Cheng <jcheng461@gmail.com>
2022-03-18 21:49:24 +0900

    function takes a word. If first character is a vowel, concatenate way to the word's end. Return this word.

--- js/scripts.js
+++ js/scripts.js
@@ -4,23 +4,24 @@
 function pigLatin(word) {
   let firstCharRemoved = word;
   let translatedWord;
-  // if (
-  //   word[0] === "a" ||
-  //   word[0] === "e" ||
-  //   word[0] === "i" ||
-  //   word[0] === "o" ||
-  //   word[0] === "u"
-  // ) {
-  // Remove character from the string.
-  firstCharRemoved = word.slice(1);
-  console.log("After removing the first character: " + firstCharRemoved);
-  // Concatenate this character to the end.
-  translatedWord = firstCharRemoved.concat(word[0]);
-  console.log("After attaching first char. to end: " + translatedWord);
-  return true;
-  // } else {
-  //   return false;
-  // }
+  // // Remove character from the string.
+  // firstCharRemoved = word.slice(1);
+  // console.log("After removing the first character: " + firstCharRemoved);
+  // // Concatenate this character to the end.
+  // translatedWord = firstCharRemoved.concat(word[0]);
+  // console.log("After attaching first char. to end: " + translatedWord);
+  if (
+    word[0] === "a" ||
+    word[0] === "e" ||
+    word[0] === "i" ||
+    word[0] === "o" ||
+    word[0] === "u"
+  ) {
+    translatedWord = word.concat("way");
+    return translatedWord;
+  } else {
+    return false;
+  }
 }
 
 // User Interface logic
