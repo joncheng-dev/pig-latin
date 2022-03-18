26ab4c962dacf2db4e62cc306d44e2a13663a9df
Jonathan Cheng <jcheng461@gmail.com>
2022-03-18 21:01:05 +0900

    add function that takes a character as parameter, and returns true if it is a vowel, and false if it is not.

---
+++ README.md
@@ -0,0 +1,5 @@
+Describe: pigLatin()
+
+Test: "It will take a character from the user, check if it is a vowel, and return true if yes, and return false if not.
+Code: pigLatin("a");
+Expected Output: "away"
---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// Utility logic
+
+// Business logic
+function pigLatin(char) {
+  if (
+    char === "a" ||
+    char === "e" ||
+    char === "i" ||
+    char === "o" ||
+    char === "u"
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// User Interface logic
